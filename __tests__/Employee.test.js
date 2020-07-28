91b8a7b907992f77a57953a1e9b5e3d400cb028d
Vanessa Lane <vlane@23andme.com>
2020-07-27 23:24:07 -0700

    Employee test and constructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,9 @@
-class Employee {}
+const Employee = require('../lib/Employee');
 
-module.exports = Employee;
+test('creates an Employee object', () => {
+    const employee = new Employee('Vanessa Lane', 'vlane0593@gmail.com', 1);
+
+    expect(employee.name).toBe('Vanessa Lane');
+    expect(employee.email).toBe('vlane0593@gmail.com');
+    expect(employee.id).toBe(1);
+});
