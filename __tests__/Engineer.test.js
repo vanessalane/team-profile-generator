ba8dd4daab8d9128558807d833f88f068ee694e8
Vanessa Lane <vlane@23andme.com>
2020-07-27 23:35:00 -0700

    improve comments/test string

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,10 +1,13 @@
 const Engineer = require('../lib/Engineer');
 
-test('creates an Engineer object', () => {
+test('creates an Engineer object that inherits from Employee', () => {
     const engineer = new Engineer('Vanessa Lane', 'vlane0593@gmail.com', 'DEF', 'vanessalane');
 
+    // if engineer has these attrs, it's inheriting from Employee.
     expect(engineer.name).toBe('Vanessa Lane');
     expect(engineer.email).toBe('vlane0593@gmail.com');
     expect(engineer.id).toBe('DEF');
+
+    // engineer-specific attr
     expect(engineer.github).toBe('vanessalane');
 })
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,10 +1,13 @@
 const Manager = require('../lib/Manager');
 
-test('creates a Manager object', () => {
+test('creates a Manager object that inherits from Employee', () => {
     const manager = new Manager('Vanessa Lane', 'vlane0593@gmail.com', 'ABC', 1);
 
+    // if manager has these attrs, it's inheriting from Employee.
     expect(manager.name).toBe('Vanessa Lane');
     expect(manager.email).toBe('vlane0593@gmail.com');
     expect(manager.id).toBe('ABC');
+
+    // test manager-specific attributes
     expect(manager.office).toBe(1);
 })
