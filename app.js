e4da7c82489d2cb30220bad33c1275340f98613e
Vanessa Lane <vlane@23andme.com>
2020-07-28 21:46:42 -0700

    add logic to build html file

--- app.js
+++ app.js
@@ -1,3 +1,177 @@
-const Team = require('./lib/Team');
 
-new Team().buildTeam();
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const generateContent = require('./src/team-template');
+const writeFile = require('./utils/generate-site');
+
+let teamMembers = [];
+
+function getEmployeePrompts(employeeType) {
+
+    // define prompts for all employees
+    const prompts = [
+        {
+            type: 'text',
+            name: 'name',
+            message: "What's their name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } 
+                else {
+                    return 'Please enter a name.';
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: "What's their email?",
+            validate: emailInput => {
+                if (!emailInput) {
+                    return 'Please enter an email';
+                }
+                else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput)) {
+                    return true;
+                } else {
+                    return 'Please enter a valid email address.';
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'id',
+            message: "What's their employee ID?",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } 
+                else {
+                    return 'Please enter an employee ID.';
+                }
+            }
+        }
+    ]
+
+    // define prompts for specific employee types
+    const typeSpecificPrompts = {
+        'engineer': {
+            type: 'text',
+            name: 'github',
+            message: "What's their github username?",
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } 
+                else {
+                    return 'Please enter a github username.';
+                }
+            }
+        },
+        'intern': {
+            type: 'text',
+            name: 'school',
+            message: "What school do they attend?",
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } 
+                else {
+                    return 'Please enter a school.';
+                }
+            }
+        },
+        'manager': {
+            type: 'text',
+            name: 'office',
+            message: "What's their office number?",
+            validate: officeNumberInput => {
+                if (officeNumberInput) {
+                    return true;
+                }
+                else {
+                    return 'Please specify an office number.';
+                }
+            }
+        }
+    };
+
+    // add the specific prompt to the prompts that should be used for this type of employee
+    prompts.push(typeSpecificPrompts[employeeType]);
+    return prompts;
+}
+
+function createEmployee(employeeType, employeeDetails) {
+
+    // get the shared attributes
+    const name = employeeDetails.name;
+    const email = employeeDetails.email;
+    const id = employeeDetails.id;
+
+    // create the relevant object and store it on the team.
+    switch(employeeType){
+        case 'engineer':
+            const github = employeeDetails.github;
+            const engineer = new Engineer(name, email, id, github);
+            teamMembers.push(engineer);
+            break;
+        case 'manager':
+            const office = employeeDetails.office;
+            const manager = new Manager(name, email, id, office);
+            teamMembers.push(manager);
+            break;
+        case 'intern':
+            const school = employeeDetails.school;
+            const intern = new Intern(name, email, id, school);
+            teamMembers.push(intern);
+            break;
+        default:
+            break;
+    }
+    console.log(`
+  The ${employeeType} ${name} was added to the team!
+    `)
+}
+
+function promptForEmployee() {
+    return inquirer
+        .prompt({
+            type: 'list',
+            message: 'What would you like to do next?',
+            name: 'nextAction',
+            choices: ['Add an engineer', 'Add an intern', 'Finish building my team']
+        })
+        .then(({nextAction}) => {
+            if (nextAction === 'Finish building my team') {
+                return teamMembers;
+            } else {
+                const employeeType = nextAction.replace('Add an ', '');
+                console.log(`
+  Got it. Let's add an ${employeeType}.
+`)
+                inquirer
+                    .prompt(getEmployeePrompts(employeeType))
+                    .then((answers) => {createEmployee(employeeType, answers)})
+                    .then(promptForEmployee);
+            }
+        })
+}
+
+function buildTeam() {
+    console.log(`
+    ----------- Let's build a team! -----------
+    `);
+    console.log(`  We'll start with the manager.
+    `)
+    return inquirer
+        .prompt(getEmployeePrompts('manager'))
+        .then(answers => {createEmployee('manager', answers)})
+        .then(promptForEmployee);
+}
+
+buildTeam()
+    .then(teamMembers => generateContent(teamMembers))
+    .then(pageHTML => writeFile(pageHTML))
+    .then(writeFileResponse => console.log(writeFileResponse));
---
+++ src/team-template.js
@@ -0,0 +1,74 @@
+const generateEmployeeCard = (employee) => {
+    if (!employee) {
+        return '';
+    }
+
+    let employeeSpecificData;
+    switch (employee.title) {
+        case 'Engineer':
+            employeeSpecificData = `<p class="card-text">GitHub: <a href="https://www.github.com/${employee.github}" class="card-link">${employee.github}</a></p>`;
+            break;
+        case 'Intern':
+            employeeSpecificData =`<p class="card-text">${employee.school}</p>`
+            break;
+        case 'Manager':
+            employeeSpecificData =`<p class="card-text">Office: ${employee.office}</p>`
+            break;
+        default:
+            break;
+    }
+
+    return `
+    <div class="col mb-4">
+        <div class="card h-100 ">
+            <div class="card-header">
+                <h4>${employee.name}</h4>
+                <p class="card-text">${employee.title}</p>
+            </div>
+            <div class="card-body font-weight-light">
+                <p><a href="mailto:${employee.email}" class="card-link">${employee.email}</a></p>
+                <p class="card-text">Employee ID: ${employee.id}</p>
+                ${employeeSpecificData}
+            </div>
+        </div>
+    </div>
+    `
+}
+
+const generatePageContent = employees => {
+    for (let i=0; i < employees.length; i++) {
+        generateEmployeeCard(employees[i]);
+    }
+}
+
+module.exports = employeeData => {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Portfolio</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+
+<body>
+    <header class="jumbotron text-center">
+        <h1 class="display-4">Hello, world!</h1>
+        <p class="lead">Meet our awesome team.</p>
+    </header>
+
+    <main class="mx-auto">
+        <div class="container font-weight-light">
+            
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+                ${generatePageContent(employeeData)};
+            </div>
+        </div>
+    </main>
+</body>
+</html>
+`
+};
